e8b20f0e26b5426cc9cd0775e353a40a70d57f01
LandonW385 <whiteboimcflyttv@gmail.com>
2024-10-29 10:28:29 -0600

    Creates struct for Task

---
+++ src/Task.py
@@ -0,0 +1,6 @@
+class Task:
+    def __init__(self, name, description, due_date, completed):
+        self.name = name
+        self.description = description
+        self.due_date = due_date
+        self.completed = completed
