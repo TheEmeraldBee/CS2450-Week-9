08d1ef7026cdf9526bcc20307710b36df3ba8a29
BradyShimanek <144560398+BradyShimanek@users.noreply.github.com>
2024-10-29 10:36:52 -0600

    Implemented delete

--- src/task_list.py
+++ src/task_list.py
@@ -1,10 +1,15 @@
 from task_creator import TaskCreator
-from Task import Task
+from task import Task
+from task_deleter import TaskDeleter
 
 
-class TaskList(TaskCreator):
+class TaskList(TaskCreator, TaskDeleter):
     def __init__(self):
         self.tasks = []
 
     def create_task(self, name, desc, due_date):
         self.tasks += [Task(name, desc, due_date, False)]
+    
+    def delete_task(self, id):
+        del self.tasks[id]
+
