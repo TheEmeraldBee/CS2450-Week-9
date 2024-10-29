5d8e3cd2e70e76ed211224e3a1c6dfb36c9e181c
brightoncox <brightonlcox@gmail.com>
2024-10-29 10:31:54 -0600

    IMplemented Create Task on TaskList

--- src/task_list.py
+++ src/task_list.py
@@ -1,3 +1,10 @@
-class TaskList:
+from task_creator import TaskCreator
+from Task import Task
+
+
+class TaskList(TaskCreator):
     def __init__(self):
-        tasks = []
+        self.tasks = []
+
+    def create_task(self, name, desc, due_date):
+        self.tasks += [Task(name, desc, due_date, False)]
